f051dce483390dc872d8ab18998812dac5eb5815
VikramMali14 <vikrammali2345@gmail.com>
2025-05-05 17:41:13 +0530

    newn

---
+++ src/Main3.java
@@ -0,0 +1,6 @@
+public class Main3 {
+    public static void main(String[] args) {
+        Client b=new Client("127.0.0.1",5000,"Vijay");
+
+    }
+}
---
+++ src/Main4.java
@@ -0,0 +1,6 @@
+public class Main4 {
+    public static void main(String[] args) {
+        Client b=new Client("127.0.0.1",5000,"markam");
+
+    }
+}
